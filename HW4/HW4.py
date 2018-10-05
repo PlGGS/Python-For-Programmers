c87b1559e025938d97ef4804000912d5c01016ed
Blake Boris <bbor0422@gmail.com>
2018-10-05 15:26:05 -0500

    Simplified HW4 #1

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -1,17 +1,11 @@
 def intersect(list1, list2):
     '''returns the intersection of two lists'''
     outputList = []
-    alreadyInOutput = False
     
     for itemA in list1:
         for itemB in list2:
             if (itemA == itemB):
-                for itemC in outputList:
-                    if (itemA == itemC):
-                        alreadyInOutput = True
-                if (alreadyInOutput == False):
-                    outputList.append(itemA)
-                    
+                outputList.append(itemA)
     return outputList
 
 def inversions(upperList):
