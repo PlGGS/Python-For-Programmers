ccb2537e5ebe5c6902468aa895d3be2bd9345111
Blake Boris <bbor0422@gmail.com>
2018-11-08 00:42:59 -0600

    Completed HW8 question 1

---
+++ HW8/HW8.py
@@ -0,0 +1,21 @@
+def Rtriangle(num, char):
+    '''Displays a triangle made up of asterisks to the screen'''
+    if num == 1:
+        print(char)
+    else:
+        Rtriangle(num - 1, char)
+        print(char * num)
+
+    pass
+
+def Rsilly(n):
+    pass
+
+def Rcount(lst, n):
+    pass
+
+def Rfilter(lst, n):
+    pass
+
+def RdeleteEveryOther(lst):
+    pass
